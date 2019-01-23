59400543e2640fecd1fee25bc753f629de85a05a
Trang <trang.frego@gmail.com>
2019-01-23 11:30:10 -0800

    Updates

---
+++ precache-manifest.13f4023fd1f76c29230fbcd0d8aad1ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c24292d5919a5f1bc2c6",
+    "url": "/donate-react/static/css/main.c57b7376.chunk.css"
+  },
+  {
+    "revision": "c24292d5919a5f1bc2c6",
+    "url": "/donate-react/static/js/main.c24292d5.chunk.js"
+  },
+  {
+    "revision": "ce908a223cb2ab9fd8e9",
+    "url": "/donate-react/static/js/1.ce908a22.chunk.js"
+  },
+  {
+    "revision": "78fdff428d6844c1a9ef",
+    "url": "/donate-react/static/js/runtime~main.78fdff42.js"
+  },
+  {
+    "revision": "aa775625238f573e4678dea780832606",
+    "url": "/donate-react/index.html"
+  }
+];
