b284cdbcbebc4f5dfa4daf6f015ac627555bb4c9
Trang <trang.frego@gmail.com>
2019-01-23 21:35:56 -0800

    create about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './About.css';
 
 const About = () => {
   return(
-    <div className="main">
-      <h2>About</h2>
+    <div className="about">
+      <h1 className="about-heading">About</h1>
+      <p className="about-text">Hello! My name is Trang. I created this app because I became a mom back in 2014 and my
+        daughter changed my whole world. I was fortunate to be able to provide for her and couldnt
+        imagine how it might feel for another struggling parent. I really wanted to connect with families
+        in Seattle who needed a lift to get back on their feet and to gift what I no longer use.
+        Also, I wanted to provide a place for families to feel welcome to reach out to their community when they
+        could use some help. I hope you feel the same and will utilize this app as a means to help those around us.</p>
     </div>
   )
 }
--- src/components/PleaseLogIn.js
+++ src/components/PleaseLogIn.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './PleaseLogIn.css';
 
 const PleaseLogIn = () => {
   return(
-    <div className="main">
-      <h3>Please Log In</h3>
+    <div className="main please-log-in">
+      <h2 className="please-log-in-text">Please Log In</h2>
     </div>
   )
 }
