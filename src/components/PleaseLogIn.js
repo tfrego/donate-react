4390f312834f95078e79600883caa1e0a7fb1b52
Trang <trang.frego@gmail.com>
2019-01-23 22:21:34 -0800

    final styling

--- src/components/PleaseLogIn.js
+++ src/components/PleaseLogIn.js
@@ -3,8 +3,9 @@ import './PleaseLogIn.css';
 
 const PleaseLogIn = () => {
   return(
-    <div className="main please-log-in">
-      <h2 className="please-log-in-text">Please Log In</h2>
+    <div className="please-log-in">
+      <h1 className="about-heading">Please Log In</h1>
+      <p className="about-text">Oh no! Please sign up or log in.</p>
     </div>
   )
 }
